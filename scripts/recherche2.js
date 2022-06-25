d81c531c6a26fa51ccb9134d90f5b74abbecc6a3
mdelacre <mdelacre@protonmail.com>
2022-06-25 23:54:58 +0200

    ajout eslint

--- scripts/recherche2.js
+++ scripts/recherche2.js
@@ -1,111 +1,111 @@
-let searchInput = document.getElementById('search');
+const searchInput = document.getElementById('search');
 let foundRecipe = [];
-let filterIngredient = [];
-let filterAppareils = [];
-let filterUstensils = [];
+const filterIngredient = [];
+const filterAppareils = [];
+const filterUstensils = [];
 let from;
 
-function defineFrom(){
-    if(foundRecipe.length == 0){
-        from = recipes;
-    }else{
-        from = foundRecipe;
-    }
+function defineFrom() {
+  if (foundRecipe.length === 0) {
+    from = recipes;
+  } else {
+    from = foundRecipe;
+  }
 }
 
-function research (tag){
-    //define from wich array we will research
-    defineFrom();
-    let fromArray = from;
-    let i = 0;
-    let y = 0;
+function research(tag) {
+  // define from wich array we will research
+  defineFrom();
+  const fromArray = from;
+  let i = 0;
+  let y = 0;
+  foundRecipe = [];
+  if (tag.length >= 3) {
+    while (i < fromArray.length) {
+      // check name and description
+      if (fromArray[i].name.toLowerCase().includes(tag.toLowerCase()) || fromArray[i].description.toLowerCase().includes(tag.toLowerCase())) {
+        if (foundRecipe.indexOf(fromArray[i]) === -1) {
+          foundRecipe.push(fromArray[i]);
+        }
+      }
+      // check every ingredients
+      while (y < fromArray[i].ingredients.length) {
+        if (fromArray[i].ingredients[y].ingredient.toLowerCase().includes(tag.toLowerCase())) {
+          if (foundRecipe.indexOf(fromArray[i]) === -1) {
+            foundRecipe.push(fromArray[i]);
+          }
+        }
+        y += 1;
+      }
+
+      /// //////////////////////////
+      // not in principal research
+      /// //////////////////////////
+
+      // check if apliance matches
+      if (fromArray[i].appliance.toLowerCase() === tag.toLowerCase()) {
+        if (foundRecipe.indexOf(fromArray[i]) === -1) {
+          foundRecipe.push(fromArray[i]);
+        }
+      }
+      // check if ustensiles matches
+      fromArray[i].ustensils.forEach((ustensil) => {
+        if (ustensil.toLowerCase().includes(tag.toLowerCase())) {
+          if (foundRecipe.indexOf(fromArray[i]) === -1) {
+            foundRecipe.push(fromArray[i]);
+          }
+        }
+      });
+
+      // next recipe
+      i += 1;
+    }
+  }
+
+  // Show found recipes
+  if (foundRecipe.length >= 1) {
+    displayRecipes(foundRecipe);
+  } else if (foundRecipe.length === 0 && tag.length >= 3) {
+    // show not found message because none recipe match the research
+    displayNotFound();
+  }
+
+  return foundRecipe;
+}
+
+function globalResearch() {
+  // check every ingredients tag
+  const tagIngredients = filterIngredient;
+  tagIngredients.forEach((tag) => {
+    research(tag);
+  });
+  // check every appareils tag
+  const tagAppareils = filterAppareils;
+  tagAppareils.forEach((tag) => {
+    research(tag);
+  });
+  // check every ustensils tag
+  const tagUstensils = filterUstensils;
+  tagUstensils.forEach((tag) => {
+    research(tag);
+  });
+
+  if (searchInput.value.length === 0 && filterAppareils.length === 0 && filterIngredient.length === 0 && filterUstensils.length === 0) {
+    displayRecipes(recipes);
     foundRecipe = [];
-    if(tag.length >= 3){
-        while(i < fromArray.length){
-            //check name and description
-            if(fromArray[i].name.toLowerCase().includes(tag.toLowerCase()) || fromArray[i].description.toLowerCase().includes(tag.toLowerCase())){
-              if(foundRecipe.indexOf(fromArray[i]) === -1){
-                foundRecipe.push(fromArray[i])
-              }
-            };
-            //check every ingredients
-            while(y < fromArray[i].ingredients.length){
-              if(fromArray[i].ingredients[y].ingredient.toLowerCase().includes(tag.toLowerCase())){
-                if(foundRecipe.indexOf(fromArray[i]) === -1){
-                    foundRecipe.push(fromArray[i])
-                }
-              }
-              y++;
-            };
-
-            /////////////////////////////
-            //not in principal research
-            /////////////////////////////
-
-            //check if apliance matches
-            if(fromArray[i].appliance.toLowerCase() == tag.toLowerCase()){
-                if(foundRecipe.indexOf(fromArray[i]) === -1){
-                    foundRecipe.push(fromArray[i]);
-                }
-            };
-            //check if ustensiles matches
-            fromArray[i].ustensils.forEach((ustensil) =>{
-                if(ustensil.toLowerCase().includes(tag.toLowerCase())){
-                    if(foundRecipe.indexOf(fromArray[i]) === -1){
-                        foundRecipe.push(fromArray[i]);
-                    }
-                }
-            });
-
-            //next recipe
-            i++;
-        };
-    }
-    
-    //Show found recipes
-    if(foundRecipe.length >= 1){
-        displayRecipes(foundRecipe);
-    }else if(foundRecipe.length == 0 && tag.length >= 3){
-        //show not found message because none recipe match the research
-        displayNotFound();
-    }
-
-    return foundRecipe;
+  }
 }
 
-function globalResearch(){
-    //check every ingredients tag
-    let tagIngredients = filterIngredient;
-    tagIngredients.forEach((tag)=>{
-        research(tag)
-    });
-    //check every appareils tag
-    let tagAppareils = filterAppareils;
-    tagAppareils.forEach((tag)=>{
-        research(tag)
-    });
-    //check every ustensils tag
-    let tagUstensils = filterUstensils;
-    tagUstensils.forEach((tag)=>{
-        research(tag)
-    });
-
-    if(searchInput.value.length == 0 && filterAppareils.length == 0 && filterIngredient.length == 0 && filterUstensils.length == 0 ){
-        displayRecipes(recipes);
-        foundRecipe = [];
-    }
-}
-
-searchInput.addEventListener('input',()=>{
-    research(searchInput.value.toLowerCase());
+searchInput.addEventListener('input', () => {
+  research(searchInput.value.toLowerCase());
+  globalResearch();
+  if (searchInput.value.length === 0) {
+    foundRecipe = [];
+    research('');
     globalResearch();
-    if(searchInput.value.length == 0){
-        foundRecipe = [];
-        research('');
-        globalResearch();
-    }
-    if(searchInput.value.length == 0 && filterAppareils.length == 0 && filterIngredient.length == 0 && filterUstensils.length == 0 ){
-        displayRecipes(recipes);
-        foundRecipe = [];
-    }
+  }
+  if (searchInput.value.length === 0 && filterAppareils.length === 0 && filterIngredient.length === 0 && filterUstensils.length === 0) {
+    displayRecipes(recipes);
+    foundRecipe = [];
+  }
 });
